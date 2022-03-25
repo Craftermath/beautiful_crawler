0f1d1410bf381f042eaaf2bf9298b804dda5ad1e
Craftermath <naousual@gmail.com>
2022-03-25 01:35:04 -0300

    feat: add the simplest link crawler

--- imprime.py
+++ imprime.py
@@ -1,20 +1,33 @@
 # imprime.py
+# Teste backend
 
-import requests, bs4
+import requests, bs4, re
+from urllib.parse import urlparse
 
 
-# res = requests.get("https://www.vultr.com/products/cloud-compute/#pricing")
+## 1 página-alvo, imprime na tela:
+# A primeira etapa exige que o seu crawler funcione para a página alvo 1,
+# capturando as informações e sendo capaz de imprimi-las na linha de comando
+# em formato arbitrário.
 
-res = requests.get("https://www.vultr.com/pricing/#cloud-compute/")
-# print(type(res))
-# print(res.status_code)
-# print("len:", len(res.text))
-# print(res.text)
+# simplest link crawler
+spec_url = "https://www.vultr.com/products/cloud-compute/#pricing"
+parsed_url = urlparse(spec_url)
+resp = requests.get(spec_url)
+for i in re.findall("<a[^>]+href=\"(.*?)\"[^>]*>(.*)?</a>", resp.text, re.I):
+    link = i[0]
+    if "cloud-compute" in link:
+        slink = "https://" + str(parsed_url.netloc + link)
+        try:
+            res = requests.get(slink)
+            break
+        except:
+            continue
 
+# info scraping
 soup = bs4.BeautifulSoup(res.text, 'html5lib')
 cloud = soup.select('#cloud-compute')[0]
 table = cloud.find_all("div", attrs={"class": "pt__row"})
-# print(table)
 
 tipos = {
         '1': 'VCPU',
@@ -30,13 +43,34 @@ for row in table:
     tipo = 0
     row_info = dict()
     for cell in cells:
-        # print(cell)  # <div class="pt__cell js-price"><strong>4.00</strong> TB<span class="is-hidden-lg-up"> Bandwidth</span></div>
         cell_info = str(str(cell).split('<strong>')[1]).split('</strong>')
         valor = cell_info[0]
         unidade = cell_info[1].split('<')[0]
         tipo +=1
         if tipo != 6:
-            cell_info = [tipos.get(str(tipo)), valor + str(unidade).removeprefix("\xa0")]
+            cell_info = [
+                    tipos.get(str(tipo)),
+                    valor + str(unidade).removeprefix("\xa0")
+                    ]
             row_info[cell_info[0]] = cell_info[1]
     table_info.append(row_info)
     print(row_info)
+
+# "formato arbitrário" é um dicionário por linha da tabela por enquanto
+
+# TODO:
+
+## 1 página-alvo, imprime na tela, salva em json
+# A segunda etapa exige que o seu crawler funcione para a mesma página-alvo da etapa
+# anterior, tendo as mesmas funcionalidades da etapa anterior, mas também sendo capaz de
+# salvar os dados em um arquivo em formato json.
+
+## 1 página-alvo, imprime na tela, salva em json, salva em csv
+# A terceira etapa exige que o seu crawler funcione para a mesma página-alvo da etapa
+# anterior, tendo as mesmas funcionalidades da etapa anterior, mas também sendo capaz de
+# salvar os dados em um arquivo em formato csv.
+
+## 2 páginas-alvo
+# A quarta etapa exige que você extraia as informações também da página-alvo 2, tendo as
+# mesmas funcionalidades da etapa anterior.
+
